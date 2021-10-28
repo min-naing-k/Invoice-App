4dda7d8966fec78fcfb562dae9c35733f0d43b32
minnaingkyaw <minnaingmdy436@gmail.com>
2021-10-29 00:23:41 +0630

    bug fix and perform realtime db and detial fix bug  reloading

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,17 @@ import { db } from '../firebase/config';
 
 export default createStore({
   state: {
+    appLoaded: true,
     invoiceData: [],
     invoiceModal: false,
     invoiceLoading: true,
+    invoiceDetail: [],
     modalActive: false,
   },
   mutations: {
+    updateAppLoaded(state) {
+      state.appLoaded = false;
+    },
     toggleInvoiceModal(state) {
       state.invoiceModal = !state.invoiceModal;
     },
@@ -19,20 +24,25 @@ export default createStore({
       state.invoiceData = result;
       state.invoiceLoading = false;
     },
+    getInvoice(state, id) {
+      state.invoiceDetail = state.invoiceData.filter((invoice) => {
+        return invoice.id === id;
+      });
+    },
   },
   actions: {
-    getInvoices({ commit }) {
+    async getInvoices({ commit }) {
       db.collection('invoices')
         .orderBy('created_at', 'desc')
         .onSnapshot((snap) => {
-          const result = [];
+          const invoices = [];
           snap.docs.forEach((doc) => {
             const document = { ...doc.data(), id: doc.id };
-            doc.data().created_at && result.push(document);
+            doc.data().created_at && invoices.push(document);
           });
-          commit('updateInvoiceData', result);
+          commit('updateInvoiceData', invoices);
+          commit('updateAppLoaded');
         });
     },
   },
-  modules: {},
 });
--- src/views/InvoiceDetail.vue
+++ src/views/InvoiceDetail.vue
@@ -1,12 +1,350 @@
 <template>
   <div>
-    <h4>Invoice Detail</h4>
+    <div v-if="invoice">
+      <div class="invoice-view container">
+        <router-link class="nav-link flex" :to="{ name: 'Home' }">
+          <img src="@/assets/icon-arrow-left.svg" alt="" /> Go Back
+        </router-link>
+        <!-- Header -->
+        <div class="header flex">
+          <div class="left flex">
+            <span>Status</span>
+            <div
+              class="status-button flex"
+              :class="{
+                paid: invoice.invoicePaid,
+                draft: invoice.invoiceDraft,
+                pending: invoice.invoicePending,
+              }"
+            >
+              <span v-if="invoice.invoicePaid">Paid</span>
+              <span v-if="invoice.invoiceDraft">Draft</span>
+              <span v-if="invoice.invoicePending">Pending</span>
+            </div>
+          </div>
+          <div class="right flex">
+            <button @click="toggleEditInvoice" class="dark-purple">Edit</button>
+            <button @click="deleteInvoice(invoice.id)" class="red">
+              Delete
+            </button>
+            <button
+              @click="updateStatusToPaid(invoice.id)"
+              v-if="invoice.invoicePending"
+              class="green"
+            >
+              Mark as Paid
+            </button>
+            <button
+              v-if="invoice.invoiceDraft || invoice.invoicePaid"
+              @click="updateStatusToPending(invoice.id)"
+              class="orange"
+            >
+              Mark as Pending
+            </button>
+          </div>
+        </div>
+        <!-- Invoice Details -->
+        <div class="invoice-details flex flex-column">
+          <div class="top flex">
+            <div class="left flex flex-column">
+              <p><span>#</span>{{ invoice.invoiceId }}</p>
+              <p>{{ invoice.productDescription }}</p>
+            </div>
+            <div class="right flex flex-column">
+              <p>{{ invoice.billerStreetAddress }}</p>
+              <p>{{ invoice.billerCity }}</p>
+              <p>{{ invoice.billerZipCode }}</p>
+              <p>{{ invoice.billerCountry }}</p>
+            </div>
+          </div>
+          <div class="middle flex">
+            <div class="payment flex flex-column">
+              <h4>Invoice Date</h4>
+              <p>
+                {{ invoice.invoiceDate }}
+              </p>
+              <h4>Payment Date</h4>
+              <p>
+                {{ invoice.paymentDueDate }}
+              </p>
+            </div>
+            <div class="bill flex flex-column">
+              <h4>Bill To</h4>
+              <p>{{ invoice.clientName }}</p>
+              <p>{{ invoice.clientStreetAddress }}</p>
+              <p>{{ invoice.clientCity }}</p>
+              <p>{{ invoice.clientZipCode }}</p>
+              <p>{{ invoice.clientCountry }}</p>
+            </div>
+            <div class="send-to flex flex-column">
+              <h4>Sent To</h4>
+              <p>{{ invoice.clientEmail }}</p>
+            </div>
+          </div>
+          <div class="bottom flex flex-column">
+            <div class="billing-items">
+              <div class="heading flex">
+                <p>Item Name</p>
+                <p>QTY</p>
+                <p>Price</p>
+                <p>Total</p>
+              </div>
+              <div
+                v-for="(item, index) in invoice.invoiceItemList"
+                :key="index"
+                class="item flex"
+              >
+                <p>{{ item.itemName }}</p>
+                <p>{{ item.qty }}</p>
+                <p>{{ item.price }}</p>
+                <p>{{ item.total }}</p>
+              </div>
+            </div>
+            <div class="total flex">
+              <p>Amount Due</p>
+              <p>{{ invoice.invoiceTotal }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="error-wrapper">
+      <p class="container error">Oops! Sorry No Invoice Here...</p>
+      <router-link class="button" :to="{ name: 'Home' }">
+        Back To Home
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const store = useStore();
+    const invoice = ref(null);
+
+    store.commit("getInvoice", props.id);
+    const data = computed(() => store.state.invoiceDetail);
+
+    if (data.value) {
+      invoice.value = data.value[0];
+    }
+
+    return {
+      invoice,
+    };
+  },
+};
 </script>
 
-<style>
+
+<style lang="scss" scoped>
+.invoice-view {
+  .nav-link {
+    margin-bottom: 32px;
+    align-items: center;
+    color: #fff;
+    font-size: 12px;
+    img {
+      margin-right: 16px;
+      width: 7px;
+      height: 10px;
+    }
+  }
+
+  .header,
+  .invoice-details {
+    background-color: #1e2139;
+    border-radius: 20px;
+  }
+
+  .header {
+    align-items: center;
+    padding: 24px 32px;
+    font-size: 12px;
+
+    .left {
+      align-items: center;
+
+      span {
+        color: #dfe3fa;
+        margin-right: 16px;
+      }
+    }
+
+    .right {
+      flex: 1;
+      justify-content: flex-end;
+
+      button {
+        color: #fff;
+      }
+    }
+  }
+
+  .invoice-details {
+    padding: 48px;
+    margin-top: 24px;
+
+    .top {
+      div {
+        color: #dfe3fa;
+        flex: 1;
+      }
+
+      .left {
+        font-size: 12px;
+        p:first-child {
+          font-size: 24px;
+          text-transform: uppercase;
+          color: #fff;
+          margin-bottom: 8px;
+        }
+
+        p:nth-child(2) {
+          font-size: 16px;
+        }
+
+        span {
+          color: #888eb0;
+        }
+      }
+
+      .right {
+        font-size: 12px;
+        align-items: flex-end;
+      }
+    }
+
+    .middle {
+      margin-top: 50px;
+      color: #dfe3fa;
+      gap: 16px;
+
+      h4 {
+        font-size: 12px;
+        font-weight: 400;
+        margin-bottom: 12px;
+      }
+
+      p {
+        font-size: 16px;
+      }
+
+      .bill,
+      .payment {
+        flex: 1;
+      }
+
+      .payment {
+        h4:nth-child(3) {
+          margin-top: 32px;
+        }
+
+        p {
+          font-weight: 600;
+        }
+      }
+
+      .bill {
+        p:nth-child(2) {
+          font-size: 16px;
+        }
+        p:nth-child(3) {
+          margin-top: auto;
+        }
+
+        p {
+          font-size: 12px;
+        }
+      }
+
+      .send-to {
+        flex: 2;
+      }
+    }
+
+    .bottom {
+      margin-top: 50px;
+
+      .billing-items {
+        padding: 32px;
+        border-radius: 20px 20px 0 0;
+        background-color: #252945;
+
+        .heading {
+          color: #dfe3fa;
+          font-size: 12px;
+          margin-bottom: 32px;
+
+          p:first-child {
+            flex: 3;
+            text-align: left;
+          }
+
+          p {
+            flex: 1;
+            text-align: right;
+          }
+        }
+
+        .item {
+          margin-bottom: 32px;
+          font-size: 13px;
+          color: #fff;
+
+          &:last-child {
+            margin-bottom: 0;
+          }
+
+          p:first-child {
+            flex: 3;
+            text-align: left;
+          }
+
+          p {
+            flex: 1;
+            text-align: right;
+          }
+        }
+      }
+
+      .total {
+        color: #fff;
+        padding: 32px;
+        background-color: rgba(12, 14, 22, 0.7);
+        align-items: center;
+        border-radius: 0 0 20px 20px;
+
+        p {
+          flex: 1;
+          font-size: 12px;
+        }
+
+        p:nth-child(2) {
+          font-size: 28px;
+          text-align: right;
+        }
+      }
+    }
+  }
+}
+.error-wrapper {
+  text-align: center;
+
+  .error {
+    font-size: 1.5rem;
+    color: #f2f2f2;
+    text-align: center;
+    margin-top: 2rem;
+  }
+  .button {
+    text-decoration: none !important;
+    background-color: #252945;
+  }
+}
 </style>
